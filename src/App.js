abcd1a3345f316c958ad554f9c9980cb2ee15170
FahriEfendi <fahriefendi095@gmail.com>
2023-12-16 19:50:19 +0700

    add auth ,dashboard

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./components/Login";
+import Dashboard from "./pages/Dashboard";
+
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+       
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
